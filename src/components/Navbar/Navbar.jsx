45e200ad005097d5d54a60a635fe4f1819205b14
Flavio Wu <flaviowu@gmail.com>
2021-10-26 17:15:05 -0300

    fix button register

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,18 +3,20 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className="wrapper__navbar">
-      <ul className="list__navbar">
-        <li className="list__navbar-item">HEALTHY RECIPES</li>
-        <li className="list__navbar-item">BLOG</li>
-        <li className="list__navbar-item">JOIN</li>
-        <li className="list__navbar-btn">
-          <Link to="/signUp">
-            <button className="register">REGISTER</button>
-          </Link>
-        </li>
-      </ul>
-    </div>
+    <>
+      <div className="wrapper__navbar">
+        <ul className="list__navbar">
+          <li className="list__navbar-item">HEALTHY RECIPES</li>
+          <li className="list__navbar-item">BLOG</li>
+          <li className="list__navbar-item">JOIN</li>
+        </ul>
+      </div>
+      <div className="button__register">
+        <Link to="/signUp">
+          <button className="register">REGISTER</button>
+        </Link>
+      </div>
+    </>
   );
 }
 
